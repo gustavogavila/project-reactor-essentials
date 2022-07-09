716abf686642103a6af6f2310eb6a50afa16deb7
Gustavo Avila <gustavogavila01@gmail.com>
2022-07-09 13:30:54 -0300

    mono subscriber test

--- src/test/java/com/github/gustavogavila/reactive/test/MonoTest.java
+++ src/test/java/com/github/gustavogavila/reactive/test/MonoTest.java
@@ -2,12 +2,21 @@ package com.github.gustavogavila.reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
 public class MonoTest {
 
     @Test
-    public void test() {
-        log.info("Tudo está funcionando!");
+    public void monoSubscriber() {
+        String name = "Gustavo";
+        Mono<String> mono = Mono.just(name).log();
+
+        mono.subscribe();
+        log.info("-------------------");
+        StepVerifier.create(mono)
+                .expectNext("Gustavo")
+                .verifyComplete();
     }
 }
