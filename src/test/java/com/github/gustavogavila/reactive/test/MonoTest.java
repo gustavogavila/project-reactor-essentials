c3abe0d63d944969f78e70a5a60f843b3bb4facb
Gustavo Avila <gustavogavila01@gmail.com>
2022-07-09 13:12:34 -0300

    Primeiro commit

---
+++ src/test/java/com/github/gustavogavila/reactive/test/MonoTest.java
@@ -0,0 +1,13 @@
+package com.github.gustavogavila.reactive.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class MonoTest {
+
+    @Test
+    public void test() {
+        log.info("Tudo está funcionando!");
+    }
+}
